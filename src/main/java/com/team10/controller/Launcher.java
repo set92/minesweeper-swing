2efaf8711474ea8e69865fb1f33ee50232efa231
Sergio E. Tobal <set.tobur@gmail.com>
2016-02-18 12:23:09 +0100

    He añadido los primeros archivos

---
+++ src/main/java/com/team10/controller/Launcher.java
@@ -0,0 +1,12 @@
+package com.team10.controller;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Launcher extends Application{
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
